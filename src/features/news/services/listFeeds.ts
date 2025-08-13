4465578b74626d078968e3cbadcef4f06a699d16
Marck-vsv <vini.marcksv@outlook.com>
2025-08-13 09:33:07 -0300

    refac: refac types

--- src/features/news/services/listFeeds.ts
+++ src/features/news/services/listFeeds.ts
@@ -1,19 +1,8 @@
-import { api } from '@/lib/http-client';
 import { endpoints } from '@/features/news/endpoints';
-
-export interface ListFeedsItem {
-  id: string;
-  name?: string;
-  url: string;
-  status: 'ACTIVE' | 'PAUSED' | string;
-  interval: string;
-  createdAt: string;
-  updatedAt: string;
-}
+import { api } from '@/lib/http-client';
+import { ListFeedsItem } from '../types';
 
 export async function listFeeds() {
   const { data } = await api.get<ListFeedsItem[]>(endpoints.news.feeds.list());
   return data;
 }
-
-
--- src/features/news/types.ts
+++ src/features/news/types.ts
@@ -8,4 +8,19 @@ export interface Feed {
   [key: string]: unknown;
 }
 
+export interface ListFeedsParams {
+  page?: number;
+  limit?: number;
+  search?: string;
+  disabled?: boolean;
+}
 
+export interface ListFeedsItem {
+  id: string;
+  name?: string;
+  url: string;
+  status: 'ACTIVE' | 'PAUSED' | string;
+  interval: string;
+  createdAt: string;
+  updatedAt: string;
+}
